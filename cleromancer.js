1b4d38a6e12f0faa5e5a3f4200ab811c79a41eb0
Adam Curtis <acurtis@spsu.edu>
2013-08-05 22:52:49 -0400

    initial commit

---
+++ cleromancer.js
@@ -0,0 +1,29 @@
+function createNewChoice() {
+    var new_choice = $('<li></li>');
+    new_choice.append('<input type="text" class="choiceField" placeholder="Insert a choice here">');
+    var delete_choice = $('<input type="button" class="deleteChoice" value="Delete choice">');
+    delete_choice.click(function(event) {
+        $(event.target).parent().remove();
+    });
+    new_choice.append(delete_choice);
+    return new_choice;
+}
+
+$(document).ready(function() {
+    var nChoices = $('#nChoices');
+    var choiceList = $('#choiceList'), resultOutput = $('#resultOutput');
+    choiceList.append(createNewChoice());
+    choiceList.append(createNewChoice());
+    $('#insertNewChoice').click(function(event) {
+        choiceList.append(createNewChoice());
+    });
+    $('#decide').click(function(event) {
+        var n = nChoices.val(), out = '';
+        for(var i = 0; i < n; i++) { 
+            var rInd = Math.floor(choiceList.children().length * Math.random());
+            out += (choiceList.find('li:eq('+rInd+') .choiceField').val() || rInd + 1) + ' ';
+        }
+        resultOutput.text(out);
+        $('.resultText').fadeIn();
+    });                                                   
+});
